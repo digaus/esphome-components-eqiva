b4519f5be9a8ceefd0e7a78a100c7c2b63585346
Dirk Gausmann <di.gaus@gmx.de>
2023-11-20 15:26:24 +0100

    Fix

--- components/eqiva_ble/eqiva_listener.cpp
+++ components/eqiva_ble/eqiva_listener.cpp
@@ -10,15 +10,14 @@ namespace eqiva_ble {
 static const char *const TAG = "eqiva_ble";
 
 bool EqivaListener::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
-        for (auto &it : device.get_manufacturer_datas()) {
-            if (it.uuid == esp32_ble_tracker::ESPBTUUID::from_uint32(0x1A00  )) {
-                ESP_LOGD(TAG, "Found Eqiva device (MAC: %s) (UUID): %s  (Name): %s", device.address_str().c_str(), it.uuid.to_string().c_str(), device.get_name().c_str());
-                return true;
-            }
+    for (auto &it : device.get_manufacturer_datas()) {
+        if (it.uuid == esp32_ble_tracker::ESPBTUUID::from_uint32(0x1A00)) {
+            ESP_LOGD(TAG, "Found Eqiva device (MAC: %s) (UUID): %s  (Name): %s", device.address_str().c_str(), it.uuid.to_string().c_str(), device.get_name().c_str());
+            return true;
         }
     }
- 
-  return false;
+
+    return false;
 }
 
 }  // namespace eqiva_ble
