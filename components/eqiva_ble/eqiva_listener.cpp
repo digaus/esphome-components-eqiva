abb12b666d61d8f143d40c05a0a38409e533ac85
Dirk Gausmann <di.gaus@gmx.de>
2023-11-20 15:07:42 +0100

    fix

--- components/eqiva_ble/eqiva_listener.cpp
+++ components/eqiva_ble/eqiva_listener.cpp
@@ -10,7 +10,7 @@ namespace eqiva_ble {
 static const char *const TAG = "eqiva_ble";
 
 bool EqivaListener::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
-    if (device.get_name().c_str() == 'KEY-BLE') {
+    if (device.get_name().c_str() == "KEY-BLE") {
         for (auto &it : device.get_manufacturer_datas()) {
         // if (it.uuid == esp32_ble_tracker::ESPBTUUID::from_raw("58e06900-15d8-11e6-b737-0002a5d5c51b")) {
             
