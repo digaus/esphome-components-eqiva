d813841dff6b744d7385414204ffe2db40846eea
Dirk Gausmann <di.gaus@gmx.de>
2023-11-20 14:36:29 +0100

    Update

--- components/eqiva_ble/eqiva_listener.cpp
+++ components/eqiva_ble/eqiva_listener.cpp
@@ -12,14 +12,7 @@ static const char *const TAG = "eqiva_ble";
 bool EqivaListener::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
   for (auto &it : device.get_manufacturer_datas()) {
     if (it.uuid == esp32_ble_tracker::ESPBTUUID::from_raw("58e06900-15d8-11e6-b737-0002a5d5c51b")) {
-      if (it.data.size() < 4)
-        continue;
-
-      uint32_t sn = it.data[0];
-      sn |= ((uint32_t) it.data[1] << 8);
-      sn |= ((uint32_t) it.data[2] << 16);
-      sn |= ((uint32_t) it.data[3] << 24);
-
+     
       ESP_LOGD(TAG, "Found Eqiva device Serial:%" PRIu32 " (MAC: %s)", sn, device.address_str().c_str());
       return true;
     }
