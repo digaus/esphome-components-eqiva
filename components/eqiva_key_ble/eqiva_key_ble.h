a6070cb06ee79110392941a6d2c2582ebec3fec2
digaus <di.gaus@gmx.de>
2026-06-04 23:59:42 +0200

    Update eqiva_key_ble.h

--- components/eqiva_key_ble/eqiva_key_ble.h
+++ components/eqiva_key_ble/eqiva_key_ble.h
@@ -161,6 +161,7 @@ class EqivaConnect : public Action<Ts...>, public Parented<EqivaKeyBle> {
             this->parent_->set_user_id(user_id);
             this->parent_->set_user_key(user_key);
             this->parent_->set_address(string_to_mac(mac_address));
+            this->parent_->connect();
             ESP_LOGD("ESP Eqiva", " Address: %s, %s", this->parent_->address_str(), mac_address.c_str());
 
         }
